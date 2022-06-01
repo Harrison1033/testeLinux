c835efdc4c7cbe1fb799fddc7a6912c64540a0de
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2022-06-01 19:02:19 -0300

    Modificação de códigos 03 e criação da classe parcelas

--- codigoJava/projetoMercado/classes/CreditoCartao.java
+++ codigoJava/projetoMercado/classes/CreditoCartao.java
@@ -1,15 +1,15 @@
 package projetoMercado.classes;
 
 public class CreditoCartao extends FormaPagamento {
-    double credito;
+    Parcelas parcelas = new Parcelas(0);
+
 
     public CreditoCartao(double valor) {
         super(valor);
     }
     public void creditoCalc(){
         if (valor > 0 && opcao == 3){
-            credito = valor + (valor * 0.08 );
-            System.out.println("O valor no crédito será: " + credito);
+            parcelas.parcelasCalc();
         }
     }
 }
--- codigoJava/projetoMercado/classes/FormaPagamento.java
+++ codigoJava/projetoMercado/classes/FormaPagamento.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class FormaPagamento extends CalculaValores{
     Scanner scan = new Scanner(System.in);
     Especie especie = new Especie(0);
+    DebitoCartao debitoCartao = new DebitoCartao(0);
+    CreditoCartao creditoCartao = new CreditoCartao(0);
     int opcao;
 
     public FormaPagamento(double valor) {
@@ -16,7 +18,13 @@ public class FormaPagamento extends CalculaValores{
         System.out.println("(1) em espécie\n(2) cartão débito\n(3) cartão crédito");
         opcao = scan.nextInt();
         if (opcao == 1){
-            especie.fPagamento();
+            especie.especieCalc();
+        }else if (opcao == 2){
+            debitoCartao.debitoCalc();
+        }else if (opcao == 3){
+            creditoCartao.creditoCalc();
+        }else {
+            System.out.println("Opção inválida: ");
         }
 
     }

2983021583cd11a31e7af09ac19f8a2f0c6d5b1d
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2022-06-01 17:56:38 -0300

    alteração do código 01

--- codigoJava/projetoMercado/classes/FormaPagamento.java
+++ codigoJava/projetoMercado/classes/FormaPagamento.java
@@ -2,14 +2,22 @@ package projetoMercado.classes;
 
 import java.util.Scanner;
 
-public class FormaPagamento {
+public class FormaPagamento extends CalculaValores{
     Scanner scan = new Scanner(System.in);
+    Especie especie = new Especie(0);
     int opcao;
+
+    public FormaPagamento(double valor) {
+        super(valor);
+    }
 
     public void fPagamento(){
         System.out.println("Qual a forma de pagamento? ");
         System.out.println("(1) em espécie\n(2) cartão débito\n(3) no pix\n(4) cartão crédito");
         opcao = scan.nextInt();
+        if (opcao == 1){
+            especie.fPagamento();
+        }
 
     }
 }

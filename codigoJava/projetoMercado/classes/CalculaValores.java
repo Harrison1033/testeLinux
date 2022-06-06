d5807ab2f57e4f126b51ee0297d7afd2cc9bfd61
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2022-06-06 16:17:59 -0300

    Alteração no códico em duas classes

--- codigoJava/projetoMercado/classes/CalculaValores.java
+++ codigoJava/projetoMercado/classes/CalculaValores.java
@@ -1,10 +1,25 @@
 package projetoMercado.classes;
 
+import java.util.Scanner;
+
 public class CalculaValores extends Produtos{
+    Scanner scan = new Scanner(System.in);
+    double item = 0;
+    double calculo = 0;
+    int opcao;
+    //String produto;
     public CalculaValores( double valor) {
         super( valor);
     }
     public void calcular(){
         //continuara o código
+        while (opcao != 3 ){
+            System.out.println("Digite o valor do produto: ");
+            item = scan.nextDouble();
+            calculo = calculo + item;
+            System.out.println("Quer adicionar um produto?\nDigite 3 para finalizar: ");
+            opcao = scan.nextInt();
+        }
+        System.out.println("O valor final é: " + calculo);
     }
 }

1ee92ea1ac0e22293109af598d1ad136702c6d7a
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2022-07-07 13:32:22 -0300

    Projeto Java para jogo retrô

---
+++ ProjetoJogo2d/Cenario.java
@@ -0,0 +1,38 @@
+package jogo;
+
+import jplay.Keyboard;
+import jplay.Scene;
+import jplay.URL;
+import jplay.Window;
+
+public class Cenario1 {
+	private Window janela;
+	private Scene cena;
+	private Jogador jogador;
+	private Keyboard teclado;
+	
+	public Cenario1 (Window window) {
+		janela = window;
+		cena = new Scene();
+		cena.loadFromFile(URL.scenario("Cenario1.scn"));
+		jogador = new Jogador(640, 350);
+		teclado = janela.getKeyboard();
+		Som.play("cidade.wav");
+		run();
+	}
+	private void run() {
+		while (true) {
+			//cena.draw();
+			jogador.mover(janela,teclado);
+			jogador.caminho(cena);
+			
+			cena.moveScene(jogador);//câmera segue o jogadeor  
+			jogador.x += cena.getXOffset();
+			jogador.y += cena.getXOffset();
+			
+			jogador.draw();
+			janela.update();
+		}
+	}
+
+}

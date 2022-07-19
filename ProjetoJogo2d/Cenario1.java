66ed06f821d8d5d04e165a0d54ca52372f2275e5
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2022-07-19 18:23:41 -0300

    Implementação da pasta de sprites e novos comentários

--- ProjetoJogo2d/Cenario1.java
+++ ProjetoJogo2d/Cenario1.java
@@ -6,22 +6,22 @@ import jplay.URL;
 import jplay.Window;
 
 public class Cenario1 extends Cenario {
-	private Window janela;
-	private Scene cena;
+	private Window janela;//criar classe janela
+	private Scene cena;//criar a classe cena 
 	private Jogador jogador;
 	private Keyboard teclado;
 	private Zumbi zumbi[];
 	
-	public Cenario1 (Window window) {
+	public Cenario1 (Window window) {//construtor com o parâmetro Window da classe main
 		janela = window;
 		cena = new Scene();
-		cena.loadFromFile(URL.scenario("Cenario1.scn"));
-		jogador = new Jogador(220, 225);
+		cena.loadFromFile(URL.scenario("Cenario1.scn"));//carrega o arquivo de cenário1
+		jogador = new Jogador(230, 225);//TENTAR UM IF PARA RETORNO DE MULTIPLAS TELAS
 		teclado = janela.getKeyboard();
 		zumbi = new Zumbi[5];//array de 5 zumbis no cenário
 		
-		Som.play("obscuro.wav");
-		run();
+		//Som.play("obscuro.wav");
+		run();//o método run contém o loop infinito como na classe main
 	}
 	public void run() {
 		 for(int i = 0; i < zumbi.length; i ++) {
@@ -54,5 +54,9 @@ public class Cenario1 extends Cenario {
 		if (tileCollision(05, jogador, cena)== true) {//05 é o número do tile que colide para outro cenário
 			new Cenario2(janela);//Abre a janela do cenario 2
 		}
+	
+	if (tileCollision(06, jogador, cena)== true) {//05 é o número do tile que colide para outro cenário
+		new Cenario3(janela);//Abre a janela do cenario 2
 	}
+}
 }
--- ProjetoJogo2d/Main.java
+++ ProjetoJogo2d/Main.java
@@ -8,16 +8,16 @@ import jplay.Window;
 public class Main {
 
 	public static void main(String[] args) {
-		Window janela = new Window(800, 600);
-		GameImage plano = new GameImage(URL.sprite("menu.png"));
-		Keyboard teclado = janela.getKeyboard();
+		Window janela = new Window(800, 600);//janela da biblioteca JPlay.jar(800,600) é o tamanho
+		GameImage plano = new GameImage(URL.sprite("menu.png"));//menu do jogo(MELHORAR ISSO DEPOIS)
+		Keyboard teclado = janela.getKeyboard();//para reconhecer o teclado(enter)para entrar na tela do jogo
 		
 		while(true) {
-			plano.draw();
-			janela.update();
+			plano.draw();//pinta a tela
+			janela.update();//fica atualizando enquanto percorre o loop
 			
-			if(teclado.keyDown(Keyboard.ENTER_KEY)) {
-				new Cenario1(janela);
+			if(teclado.keyDown(Keyboard.ENTER_KEY)) {//reconhece o enter com a tecla para entrar no jogo
+				new Cenario1(janela);//diz em qual cenário entrar após digitar o enter
 			}
 		}
 	}
